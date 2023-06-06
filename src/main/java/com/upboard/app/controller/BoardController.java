acc2e044c6b63beb1b4f5aea51eecf9c0a651a23
eunjiProgramming <estelle.programming@gmail.com>
2023-06-06 20:53:22 +0900

    게시판 읽기, 쓰기, 삭제, 수정 기능 구현(최종)

--- src/main/java/com/upboard/app/controller/BoardController.java
+++ src/main/java/com/upboard/app/controller/BoardController.java
@@ -9,10 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,100 +28,109 @@ public class BoardController {
     BoardService boardService;
 
     @PostMapping("/modify")
-    public String modify(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rattr) {
+    public String modify(BoardDto boardDto, Integer page, Integer pageSize, RedirectAttributes rattr, Model m, HttpSession session) {
+
         String writer = (String)session.getAttribute("id");
         boardDto.setWriter(writer);
+
         try {
-            int rowCnt = boardService.modify(boardDto); // insert
+            if (boardService.modify(boardDto)!= 1)
+                throw new Exception("Modify failed.");
 
-            if(rowCnt != 1)
-                throw new Exception("Modify failed");
-
+            rattr.addAttribute("page", page);
+            rattr.addAttribute("pageSize", pageSize);
             rattr.addFlashAttribute("msg", "MOD_OK");
-
             return "redirect:/board/list";
         } catch (Exception e) {
             e.printStackTrace();
             m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
             m.addAttribute("msg", "MOD_ERR");
-            return "board";
-        }
-    }
-
-    @PostMapping("/write")
-    public String write(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rattr) {
-        String writer = (String)session.getAttribute("id");
-        boardDto.setWriter(writer);
-        try {
-            int rowCnt = boardService.write(boardDto); // insert
-
-            if(rowCnt != 1)
-                throw new Exception("Write failed");
-
-            rattr.addFlashAttribute("msg", "WRT_OK");
-
-            return "redirect:/board/list";
-        } catch (Exception e) {
-            e.printStackTrace();
-            m.addAttribute(boardDto);
-            m.addAttribute("msg", "WRT_ERR");
-            return "board";
+            return "board"; // 등록하려던 내용을 보여줘야 함.
         }
     }
 
     @GetMapping("/write")
     public String write(Model m) {
         m.addAttribute("mode", "new");
-        return "board"; // 읽기와 쓰기에 사용. 쓰기에 사용할 때는 mode=new
+
+        return "board";
     }
 
-    @PostMapping("/remove")
-    public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
-        String writer = (String) session.getAttribute("id");
+    @PostMapping("/write") // insert니까 delete인 remove하고 동일
+    public String write(BoardDto boardDto, RedirectAttributes rattr, Model m, HttpSession session) {
+        String writer = (String)session.getAttribute("id");
+        boardDto.setWriter(writer);
+
         try {
-            m.addAttribute("page", page); // 자동으로 리다이렉트할 때 붙는다
-            m.addAttribute("pageSize", pageSize); // 자동으로 리다이렉트할 때 붙는다
+            if (boardService.write(boardDto) != 1)
+                throw new Exception("Write failed.");
 
-            int rowCnt = boardService.remove(bno, writer);
-
-            if(rowCnt != 1) throw new Exception("board remove error");
-
-            rattr.addFlashAttribute("msg", "DEL_OK");
-
-
+            rattr.addFlashAttribute("msg", "WRT_OK");
+            return "redirect:/board/list";
         } catch (Exception e) {
             e.printStackTrace();
-            rattr.addFlashAttribute("msg", "DEL_ERR");
+            m.addAttribute("mode", "new"); // 글쓰기 모드로
+            m.addAttribute(boardDto);      // 등록하려던 내용을 보여줘야 함.
+            m.addAttribute("msg", "WRT_ERR");
+            return "board";
         }
-
-        return "redirect:/board/list";
     }
 
     @GetMapping("/read")
-    public String read(Integer bno, Integer page, Integer pageSize, Model m) {
+    public String read(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, Model m) {
         try {
             BoardDto boardDto = boardService.read(bno);
-            // m.addAttribute("boardDto", boardDto); // 아래 문장과 동일
             m.addAttribute(boardDto);
             m.addAttribute("page", page);
             m.addAttribute("pageSize", pageSize);
         } catch (Exception e) {
             e.printStackTrace();
+            rattr.addAttribute("page", page);
+            rattr.addAttribute("pageSize", pageSize);
+            rattr.addFlashAttribute("msg", "READ_ERR");
+            return "redirect:/board/list";
         }
+
         return "board";
     }
 
-    @GetMapping("/list")
-    public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
-        if(!loginCheck(request))
-            return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, HttpSession session) {
+        String writer = (String)session.getAttribute("id");
+        String msg = "DEL_OK";
 
-        if (page == null) page = 1;
-        if (pageSize == null) pageSize=10;
+        try {
+            if(boardService.remove(bno, writer)!=1)
+                throw new Exception("Delete failed.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            msg = "DEL_ERR";
+        }
+
+        rattr.addAttribute("page", page);
+        rattr.addAttribute("pageSize", pageSize);
+        rattr.addFlashAttribute("msg", msg);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/list")
+    public String list(@RequestParam(defaultValue ="1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer pageSize,Model m, HttpServletRequest request) {
+        if(!loginCheck(request))
+            return "redirect:/login/login?toURL="+request.getRequestURL(); // 로그인을 안했으면 로그인 화면으로 이동
 
         try {
             int totalCnt = boardService.getCount();
+            m.addAttribute("totalCnt", totalCnt);
+
             PageHandler pageHandler = new PageHandler(totalCnt, page, pageSize);
+
+            if(page < 0 || page > pageHandler.getTotalPage())
+                page = 1;
+            if(pageSize < 0 || pageSize > 50)
+                pageSize = 10;
 
             Map map = new HashMap();
             map.put("offset", (page-1)*pageSize);
@@ -126,19 +139,22 @@ public class BoardController {
             List<BoardDto> list = boardService.getPage(map);
             m.addAttribute("list", list);
             m.addAttribute("ph", pageHandler);
-            m.addAttribute("page", page);
-            m.addAttribute("pageSize", pageSize);
+
+            Instant startOfToday = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant();
+            m.addAttribute("startOfToday", startOfToday.toEpochMilli());
         } catch (Exception e) {
             e.printStackTrace();
+            m.addAttribute("msg", "LIST_ERR");
+            m.addAttribute("totalCnt", 0);
         }
 
         return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
     }
 
     private boolean loginCheck(HttpServletRequest request) {
-        // 1. 세션을 얻어서
-        HttpSession session = request.getSession();
+        // 1. 세션을 얻어서(false는 session이 없어도 새로 생성하지 않는다. 반환값 null)
+        HttpSession session = request.getSession(false);
         // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
-        return session.getAttribute("id")!=null;
+        return session!=null && session.getAttribute("id")!=null;
     }
 }
